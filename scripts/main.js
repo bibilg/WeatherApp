ec850e7f55ec827c946b00d2d12c50c238f5ee5c
bibilg <58175738+bibilg@users.noreply.github.com>
2021-05-28 09:11:06 +0200

    Change block-info according to the API

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 const apiKey = '3e6a22803933489364840e4fc5bb1ef8';
+const temps = document.querySelector('.temps');
+const temperature = document.querySelector('.temperature');
+const localisation = document.querySelector('.localisation');
+const logo = document.querySelector('.logo-meteo');
 
 navigator.geolocation.getCurrentPosition( position => {
     
@@ -8,22 +12,37 @@ navigator.geolocation.getCurrentPosition( position => {
     apiCall(latitude,longitude);
 
 }, () => {
-    alert('Vous devez accpeter la géolocalisation');
+    alert('Vous devez accepter la géolocalisation');
     return;
 })
 
 function apiCall(latitude, longitude){
 
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&units=metric&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&units=metric&lang=fr&appid=${apiKey}`)
     .then((res) => {
         if(res.ok){
             return res.json();
         }
     })
     .then((value) => {
-        console.log(value);
+
+        editBlockInfo(value);
+        
     })
     .catch(function(err){
         console.log('Erreur :' + err);
     })
+}
+
+function editBlockInfo(value){
+
+    if( value.current.weather[0].icon.includes('d') ){
+        logo.setAttribute('src',`./ressources/jour/${value.current.weather[0].icon}.svg`)
+    }else{
+        logo.setAttribute('src',`./ressources/nuit/${value.current.weather[0].icon}.svg`)
+    }
+
+    temps.innerText = value.current.weather[0].description;
+    temperature.innerText = Math.round(value.current.temp) + '°';
+    localisation.innerText = value.timezone;
 }
